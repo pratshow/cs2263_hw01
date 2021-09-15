9cc66274e58b1f37b5edbc7c7493ea69a8b5b530
pratshow <pratshow@isu.edu>
2021-09-14 21:54:54 -0600

    Full output file implementation

--- app/src/main/java/edu/isu/cs2263/hw01/ConsoleInput.java
+++ app/src/main/java/edu/isu/cs2263/hw01/ConsoleInput.java
@@ -1,3 +1,8 @@
+/*
+Show Pratoomratana - CS2263 - HW1
+V1.2.0
+*/
+
 package edu.isu.cs2263.hw01;
 
 import java.util.Scanner;
@@ -16,7 +21,7 @@ public class ConsoleInput implements Input {
   }
 
   //If no argument is given enter "live mode".
-  public void loopCli(Options options) throws ParseException, FileNotFoundException, IOException{
+  public void loopCli(Options options, String toFile) throws ParseException, FileNotFoundException, IOException{
     App app = new App();
     //Making a scanner for live cmd line with whitespace as a delimiter.
     Scanner scanner = new Scanner(System.in);
@@ -28,6 +33,13 @@ public class ConsoleInput implements Input {
     try{ //Try catch block here because if inputArr[0] is a '-h' etc. you get an error since it's a string not an int.
         int total = app.eval(inputArr, 0);
         app.printTotal(total);
+        if ((toFile != null)) { // Sees if the `-o` flag was passed
+          FileWriter fWriter = new FileWriter(toFile, true);
+          fWriter.write(input + "\n" + "   ->" + total + "\n"); //Write to file and flush buffer
+          fWriter.flush();
+          fWriter.close();
+          loopCli(options, toFile); // loop via recursion
+        }
       }
     catch (Exception e){ //Otherwise a diffrent command was entered
       app.checkCmd(options, inputArr);
@@ -35,7 +47,7 @@ public class ConsoleInput implements Input {
     finally{
       //scanner.close();
       //Looping via recurrsion use 'ctrl + c to exit'
-      app.loopInput(options);
+      loopCli(options, null);
     }
   }
 }
---
+++ app/src/main/java/edu/isu/cs2263/hw01/Output.java
@@ -0,0 +1,11 @@
+/*
+Show Pratoomratana - CS2263 - HW1
+V1.2.0
+*/
+
+package edu.isu.cs2263.hw01;
+
+// Interface for the --output flag
+interface Output{
+  void getOutput(String expr, String result);
+}
